aac71a001db62730cc0a18addff8d052d1486107
KevinHOARAU974 <92148526+KevinHOARAU974@users.noreply.github.com>
2025-06-03 08:40:41 +0200

    correction

--- examples/transformers/prune_timm_vit.py
+++ examples/transformers/prune_timm_vit.py
@@ -124,7 +124,7 @@ def main(args):
     # example_inputs = torch.randn(1, *input_size).to(device)
     # base_macs, base_params = tp.utils.count_ops_and_params(model, example_inputs)
     
-    if args.model_name == "vit-b-16":
+    if args.model_name == "vit_base_patch16_224":
         model = timm.create_model(args.model_name, pretrained=True, num_classes=args.num_classes)
         pretrained = torch.load(args.model_path, map_location='cpu')
         if 'model' in pretrained:
